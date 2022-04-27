d1c6d03fc2d98dcbf2fcb18f05c56e2a7af838f2
admin <admin@Bcalab2-m01>
2022-04-27 10:14:07 +0530

    First Commit.

---
+++ Ques1/src/phoneBill.java
@@ -0,0 +1,13 @@
+
+public class phoneBill {
+	
+	String acct_num;
+	int num_units;
+	
+	phoneBill(String acct_num , int _num)
+	{
+		this.acct_num = acct_num;
+		this.num_units = num_units;
+	}
+
+}
